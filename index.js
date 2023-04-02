798b8073f2e906f2d0577504b01923a1dc7f06ad
Cathy T Bradley <cathytbradley116@gmail.com>
2023-04-02 16:12:49 -0700

    add name and blurb

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 window.onload = function () {
 
+    const h1 = document.createElement("h1")
+
     fetch('./userData.json')
         .then((reponse) => reponse.json())
         .then((json) => {
             
           document.querySelector("#profileImage").setAttribute("src", json.profileImage.path)
+
+          document.getElementById("name").innerHTML = json.about.name
+          document.getElementById("blurb").innerHTML = json.about.blurb
           
           document.querySelector("#emailSignup").setAttribute("src", json.emailSignup.src)
             

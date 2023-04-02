c0921182ce5ffbbe6c4e65d8a51882793864761b
Cathy T Bradley <cathytbradley116@gmail.com>
2023-04-02 16:53:45 -0700

    add buttons to loop

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 window.onload = function () {
 
-    const h1 = document.createElement("h1")
 
     fetch('./userData.json')
         .then((reponse) => reponse.json())
@@ -10,6 +9,18 @@ window.onload = function () {
 
           document.getElementById("name").innerHTML = json.about.name
           document.getElementById("blurb").innerHTML = json.about.blurb
+          
+          json.socials.forEach(social => {
+            const btn = document.createElement("button")
+            let textNode = document.createTextNode(social.siteName)
+
+            btn.setAttribute("type", "button")
+            btn.setAttribute("onclick", "location.href='" + social.siteUrl + "';")
+            btn.setAttribute("value", "Go to " + social.siteName)
+            btn.appendChild(textNode)
+
+            document.getElementById("socials").appendChild(btn)
+          });
           
           document.querySelector("#emailSignup").setAttribute("src", json.emailSignup.src)
             
